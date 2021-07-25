99ecf843ab4f28822a8f6b5e0439ca78ab486c09
Paolo Ramos <ing.paolo.ramos@gmail.com>
2021-07-25 14:29:03 -0500

    6

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY', ' Zabrinna !!!' ],
+			strings: [ 'HAPPY', 'BIRTHDAY', ' Zabrinna !!!', 'Enjoy!' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
